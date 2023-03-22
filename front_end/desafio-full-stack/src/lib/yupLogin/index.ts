2379897621efa006ef52167a1f3b4ca5ddcb5e44
arimoncaojr <106547012+arimoncaojr@users.noreply.github.com>
2023-03-22 20:29:33 -0300

    fix: delete fold full_stack

--- front_end/desafio-full-stack/src/lib/yupLogin/index.ts
+++
@@ -1,6 +0,0 @@
-import * as yup from "yup";
-
-export const schema = yup.object({
-  email: yup.string().email("Email inválido").required("Email não preenchido"),
-  password: yup.string().required("Senha não preenchida"),
-});
